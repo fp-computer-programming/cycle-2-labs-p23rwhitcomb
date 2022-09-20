3c0ef45da5529b68097efde3c0142e5d20d50484
Richie Whitcomb <p23rwhitcomb@fairfieldprep.org>
2022-09-20 13:44:09 -0400

    Lab

---
+++ lab_2-6.py
@@ -0,0 +1,26 @@
+#creator RW 9/20/2022
+
+a = 5
+b = 10
+c = 0
+d = -4
+
+if a % 2 == 0:
+    print('number is even')
+else:
+    print('number is odd')
+
+if b % 2 == 0:
+    print('number is even')
+else:
+    print('number is odd')
+
+if c % 2 == 0:
+    print('number is even')
+else:
+    print('number is odd')
+
+if d % 2 == 0:
+    print('number is even')
+else:
+    print('number is odd')
